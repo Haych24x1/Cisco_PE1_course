ba31d9f5d5d902ff2372c868ce5084396c854773
Haych24x1 <hamzanawaz24x@gmail.com>
2024-12-17 23:16:31 +0000

    Cisco Section 2, 2.2 Python Iterals

--- python_essentials_1/Cisco_PE1.py
+++ python_essentials_1/Cisco_PE1.py
@@ -134,4 +134,33 @@ print("James Bond.\n")
 
 # Line 5 will raise SyntaxError, because the ' symbol in the Greg's book. string requires an escape character.
 
+# Literals - the data in itself
+print("2") # String
+print(2) # Int
 
+# Integers
+print(0o123) # Octal Number Representation  - Only take numbers from 0-7 if 0o is in the number
+print(0x123) # Hexadecimal Representation - It's a number with a decimal that value is equals to 291.
+print("\n")
+# You can tell the difference 0o is for Octal number. 0x is for Hexadecimal
+
+# Floats
+# Decimal point is essential for recognizing floating-point numbers within python
+# Int vs Floats
+print(int(4)) # Int
+print(float(4.0), "\n") #Float
+
+# Coding floats
+print(0.0000000000000000000001, "\n")
+# Python always chooses the more economical form of the number's presentation, 
+# and you should take this into consideration when creating literals.
+
+# Strings
+print("I like \"Monty Python\", \n")
+
+# Python can use an apostrophe instead of a quote. Either of these characters may delimit strings, but you must be consistent
+print('I like "Monty Python"')
+
+# Coding strings
+print('I\'m Monty Python.\n')
+print("I'm Monty Python.")
